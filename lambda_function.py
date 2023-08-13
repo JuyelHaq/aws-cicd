fb497f391f4b34a9b60fa1830867fbc895b40564
Juyel <juye@thirdeyedata.ai>
2023-08-13 14:26:55 +0530

    Demo

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'Col_Number1': [1,2], 'Col_Number2': [3,4]}
+    d = {'Col_Number1': [1, 2], 'Col_Number2': [3, 4]}
     df = pd.DataFrame(data=d)
+    
+    # Adding more columns to the DataFrame
+    df['Col_Number3'] = [5, 6]
+    df['Col_Number4'] = [7, 8]
+    
+    print("Modified DataFrame:")
     print(df)
+    
     print('Finished')
